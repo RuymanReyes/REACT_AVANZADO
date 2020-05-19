fd909d8f2f7c98027c8be001f26a52d2bd1a77a6
Ruyman Reyes <Ruymanr@gmail.com>
2020-05-19 12:49:18 +0200

    cambios gitignore

--- producthunt-next/firebase/config.js
+++ producthunt-next/firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCrD5CtVcJlvQADfdLXc1n3GdTaKemZwY8",
+  authDomain: "product-hunt-f7055.firebaseapp.com",
+  databaseURL: "https://product-hunt-f7055.firebaseio.com",
+  projectId: "product-hunt-f7055",
+  storageBucket: "product-hunt-f7055.appspot.com",
+  messagingSenderId: "1070549685372",
+  appId: "1:1070549685372:web:df2f0c8848986ed31fa5f9"
 };
 
   export default firebaseConfig;
